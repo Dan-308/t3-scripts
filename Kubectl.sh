a5055a1d28055f9277650e3a1f20751cfc2e4554
sara-albarawi <sara.albarawi@gmail.com>
2024-07-25 10:54:02 +0100

    Update Kubectl.sh

--- Kubectl.sh
+++ Kubectl.sh
@@ -2,4 +2,4 @@ curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s http
 chmod +x kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 kubectl version
-gcloud auth login -y
+gcloud auth login 
